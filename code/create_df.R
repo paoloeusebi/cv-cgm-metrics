61a7702cbd2d4f48b0df86bce350e492670e904f
Paolo Eusebi <paoloeusebir@gmail.com>
2025-08-08 11:14:02 +0200

    Added R code

---
+++ code/create_df.R
@@ -0,0 +1,14 @@
+library(readr)
+library(dplyr)
+# Create a data frame from a CSV file
+# skip first row
+df <- read_csv("data/f_cgm_data_2025-08-08.csv", skip = 1) |>
+  rename(mgdL=`Storico del glucosio mg/dL`,
+         time=`Timestamp del dispositivo`,
+         device=Dispositivo) |>
+  select(device, time, mgdL) |>
+  tail(-6) |>
+  mutate(time=as.POSIXct(time, format="%d/%m/%Y %H:%M:%S", tz="UTC"))
+ds <- df |>
+  mutate(time=as.POSIXct(time, format = "%d-%m-%Y %H:%M"))
+ds 
---
+++ code/main.R
@@ -0,0 +1,1 @@
+source("code/create_df.R")
